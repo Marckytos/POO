3da37238ce6d5d59705eb60b5b919dbf83820910
Marckytos <marquillos.bros3451@gmail.com>
2023-03-02 12:21:18 -0600

    Automovil.java

---
+++ src/AutomovilDriver.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class AutomovilDriver {
+    public static void main(String[] args) {
+        Automovil auto1 = new Automovil();
+        Automovil auto2 = new Automovil();
+        System.out.println("El auto es:");
+        int edad = 12;
+        Automovil auto3 = new Automovil("TOYOTA","Supra",(byte)15, Color.BLUE);
+        System.out.println(auto3);
+        Automovil auto4 = new Automovil(Color.RED);
+        System.out.println(auto4);
+        auto1.setMarca("HONDA");
+        System.out.println("La marca es:" + auto1.getMarca());
+
+        //auto1.marca = "Una marca que no existe porque así se me ocurrio";
+        auto1.setSubMarca("Una marca que no existe");
+        auto1.setMarca("TEsLa");
+        System.out.println("Fin");
+
+    }
+}
