7c27cdb187f92bb44b4e68cdb567f88bd7a76992
Marckytos <114682944+Marckytos@users.noreply.github.com>
2023-04-24 19:51:57 -0600

    Add files via upload

---
+++ src/Motocicleta.java
@@ -0,0 +1,64 @@
+public class Motocicleta implements VehiculoI {
+    private int centimetrosCubicos;
+    private String marca;
+
+    public Motocicleta() {
+    }
+
+    public int getCentimetrosCubicos() {
+        return centimetrosCubicos;
+    }
+
+    public void setCentimetrosCubicos(int centimetrosCubicos) {
+        this.centimetrosCubicos = centimetrosCubicos;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Motocicleta(int centimetrosCubicos, String marca) {
+
+        this.centimetrosCubicos = centimetrosCubicos;
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Motocicleta{" +
+                "centimetrosCubicos=" + centimetrosCubicos +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
+    public void hacerCaballito(){
+        System.out.println("Haciendo un caballito para romperme las costillas xd");
+    }
+
+    @Override
+    public boolean encender() {
+        System.out.println("Encendiendo la motocicleta marca " + this.marca);
+        return true;
+    }
+
+    @Override
+    public boolean apagar() {
+        System.out.println("Apagando la motocicleta marca " + this.marca);
+        return true;
+    }
+
+    @Override
+    public void avanzar() {
+        System.out.println("Avanzando con la motocicleta de " + this.centimetrosCubicos + "CC");
+
+    }
+
+    @Override
+    public void frenar() {
+        System.out.println("Frenando con la motocicleta de " + this.centimetrosCubicos + "CC");
+
+    }
+}
